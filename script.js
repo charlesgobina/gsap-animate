77f0aa40afa83449597f4aa5777729cc11b89336
charlesgobina <charles.gobina@gmail.com>
2025-01-02 23:52:03 +0200

    chore: animate with to, from and fromto

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
-console.log("hello world")
-gsap.to('h1', {x:400})
+// console.log("hello world")
+// gsap.to('h1', {x:400, duration:3})
+// gsap.to(".fred", {
+//   x:700,
+//   y:400,
+//   scale:3,
+//   rotation: 360,
+//   duration: 3
+// })
+
+// gsap.from(".fred", {
+//   x:700,
+//   y:400,
+//   scale:3,
+//   rotation: 360,
+//   duration: 3
+// })
+
+// gsap.fromTo(".fred", {
+//   x:700,
+//   y:400,
+//   opacity:0
+// }, {
+//   x:400,
+//   y:200,
+//   scale:3,
+//   opacity:1,
+//   duration:3
+// })
+
+gsap.to(".big", {x:400, duration:3, repeat:-1, yoyo:true})
+gsap.to(".small", {x:500, duration:2})
+

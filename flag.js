4a1efd55a31742cb30fa8069ef9ade40617af9a9
charlesgobina <charles.gobina@gmail.com>
2025-01-05 23:41:27 +0200

    flag attempt

---
+++ flag.js
@@ -0,0 +1,33 @@
+// const redTeam = document.querySelector(".red");
+// const greenTeam = document.querySelector(".green");
+
+// console.log("Kaili is here");
+
+// console.log(gsap);
+
+// gsap.to("#upper div", {
+//   y: 20,
+//   duration: 1,
+//   ease: "sine",
+//   stagger: {
+//     amount: 1,
+//     from: "edge"
+//   },
+//   repeat: -1,
+//   yoyo: true
+// })
+
+
+// gsap.to("#lower div", {
+//   y: 20,
+//   duration: 1,
+//   ease: "sine",
+//   stagger: {
+//     amount: 1,
+//     from: "edge"
+//   },
+//   repeat: -1,
+//   yoyo: true
+
+// })
+
